c9d36edfe62f35e0d569153ab4d6338fdd6d6ae3
eutimio tatis <eutimio.tatis.mt@correo.usa.edu.co>
2021-10-12 22:17:12 -0500

    base para realziar el Reto 3

---
+++ Reto3.MinTic.USA/src/test/java/Reto3/MinTic/USA/Reto3/MinTic/USA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto3.MinTic.USA.Reto3.MinTic.USA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
